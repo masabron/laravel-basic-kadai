691db620cc7ed570935295bc989b0ce1540d9b53
samurai-tutorial <GitHub>
2024-09-21 22:17:53 +0900

    Initial commit

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB; // クエリビルダを使用するために追加
+use App\Models\Post;
 
 class PostController extends Controller {
     public function index() {
@@ -14,5 +15,12 @@ class PostController extends Controller {
 
         // ビューにデータを渡す
         return view('post.index',compact('posts'));
+    }
+    public function show($id) {
+        // URL'/posts/{id}'の'{id}'部分と主キー（idカラム）の値が一致するデータをpostsテーブルから取得し、変数$postに代入する
+        $post = Post::find($id);
+
+        // 変数$productをproducts/show.blade.phpファイルに渡す
+        return view('post.show', compact('post'));
     }
 }
